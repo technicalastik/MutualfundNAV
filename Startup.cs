ed76c52e9460140d169930d8e065d572b5be6836
technicalastik <51930515+technicalastik@users.noreply.github.com>
2022-03-22 19:54:06 +0530

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+
+
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,6 +15,7 @@ using DailyMutualFundNAVMicroservice.Repository;
 using DailyMutualFundNAVMicroservice.Provider;
 using Microsoft.EntityFrameworkCore;
 using DailyMutualFundNAVMicroservice.Models;
+using Microsoft.OpenApi.Models;
 
 namespace DailyMutualFundNAVMicroservice
 {
@@ -24,14 +27,18 @@ namespace DailyMutualFundNAVMicroservice
         }
 
         public IConfiguration Configuration { get; }
+        public object JwtBearerDefaults { get; private set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
             services.AddScoped<IMutualFundRepository, MutualFundRepository>();
             services.AddScoped<IMutualFundProvider, MutualFundProvider>();
             services.AddDbContext<MutualFundDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            
+           
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
@@ -40,6 +47,31 @@ namespace DailyMutualFundNAVMicroservice
                     Title = "Daily Mutual Fund NAV",
                     Description = "Daily Mutual Fund NAV using Swagger"
                 });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                     {
+                         {
+                               new OpenApiSecurityScheme
+                                 {
+                                     Reference = new OpenApiReference
+                                     {
+                                         Type = ReferenceType.SecurityScheme,
+                                         Id = "Bearer"
+                                     }
+                                 },
+                                 new string[] {}
+                         }
+                     });
+
+
             });
         }
 
